bc8d74b0649e31965fe250fa59ab6ebcceebdd61
osnaldy <osnaldy@hotmail.com>
2017-09-18 13:55:51 -0400

    Traverse through Linked Listd

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -1,8 +1,27 @@
 public class SinglyLinkedList {
 
     // SinglyLinkedList class will have ListNode as instance variable
-    private ListNode head; //this will be the head of the linkedList
+    //private ListNode head; //this will be the head of the linkedList
 
+    //Given a List write a function that prints elements it holds
+
+    public void display(ListNode head) {
+
+        //First we need to check if the head is null as they will not be any elements to print
+
+        if (head == null)
+        {
+            return;
+        }
+        ListNode current = head;
+        // wi will look each element in the list until it ends as last node will point to null
+        while (current != null) {
+            System.out.print(current.data + " --> "); //print current element
+
+            current = current.next; //here we move current node to the next node
+        }
+        System.out.println(current); // here it will be null
+    }
     //It contains a static Inner class ListNode
     private static class ListNode {
 
@@ -33,5 +52,9 @@ public class SinglyLinkedList {
         second.next = thrid; //10 --> 8 --> 1
         thrid.next = fourth; //10 --> 8 --> 1 --> 11
         fourth.next = null; //10 --> 8 --> 1 --> 11 --> null
+
+        //to text it, we create an object from the main class
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        singlyLinkedList.display(head);
     }
 }
