ff890cde77f74a546dd71e3fbcc3e2bf37b6dd56
osnaldy <osnaldy@hotmail.com>
2017-09-30 13:44:25 -0400

    Working with Array

---
+++ src/Arrays.java
@@ -0,0 +1,39 @@
+public class Arrays {
+
+    // this integer array will contain 50 boxes
+    private int[] theArray = new int[50];
+
+    // here will only print the part of the array that has values
+    private int arraySize  = 10;
+
+    //generate a random number of values
+    public void generateRandomArray() {
+
+        for (int i = 0; i < arraySize; i++) {
+
+            theArray[i] = (int)(Math.random()*10)+10;
+        }
+    }
+
+    //Function to print the array
+
+    public void printArray() {
+
+        System.out.println("----------");
+        for (int i = 0; i < arraySize; i++) {
+
+            System.out.print("| " + i + " | ");
+            System.out.println(theArray[i] + " |");
+            System.out.println("----------");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Arrays theArray = new Arrays();
+
+        theArray.generateRandomArray();
+        theArray.printArray();
+
+    }
+}
